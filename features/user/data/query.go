534367f79f23b9885131d9e4a1849cfdb640d7e2
pradanadp <pradana.dimasdp@gmail.com>
2023-06-22 08:24:09 +0700

    Update user query

--- features/user/data/query.go
+++ features/user/data/query.go
@@ -2,8 +2,10 @@ package data
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 
+	"github.com/playground-pro-project/playground-pro-api/app/middlewares"
 	"github.com/playground-pro-project/playground-pro-api/features/user"
 	"github.com/playground-pro-project/playground-pro-api/utils/helper"
 	"gorm.io/gorm"
@@ -15,12 +17,19 @@ type userQuery struct {
 
 // Create implements user.UserData.
 func (uq userQuery) Create(user user.UserEntity) (string, error) {
+	// Convert UserEntity to UserModel
 	userModel := UserEntityToModel(user)
+
+	// Hash the password
 	userModel.Password = helper.HashPass(userModel.Password)
+
+	// Generate a unique user ID
 	userModel.UserID = helper.GenerateUserID()
 
+	// Insert the user into the database
 	createResult := uq.db.Create(&userModel)
 	if createResult.Error != nil {
+		// Handle specific errors related to email and phone uniqueness
 		switch {
 		case strings.Contains(createResult.Error.Error(), "email"):
 			return "", errors.New("email already in use")
@@ -30,16 +39,26 @@ func (uq userQuery) Create(user user.UserEntity) (string, error) {
 		return "", createResult.Error
 	}
 
+	// Check if any row was affected during the insertion
 	if createResult.RowsAffected == 0 {
 		return "", errors.New("failed to insert, row affected is 0")
 	}
 
+	// Return the generated user ID
 	return userModel.UserID, nil
 }
 
 // DeleteByID implements user.UserData.
 func (uq userQuery) DeleteByID(userID string) error {
-	panic("unimplemented")
+	deleteResult := uq.db.Delete(&User{}, userID)
+	if deleteResult.Error != nil {
+		return fmt.Errorf("failed to delete user: %w", deleteResult.Error)
+	}
+	if deleteResult.RowsAffected == 0 {
+		return fmt.Errorf("no user found with ID: %s", userID)
+	}
+
+	return nil
 }
 
 // GetAll implements user.UserData.
@@ -49,17 +68,77 @@ func (uq userQuery) GetAll() ([]user.UserEntity, error) {
 
 // GetByID implements user.UserData.
 func (uq userQuery) GetByID(userID string) (user.UserEntity, error) {
-	panic("unimplemented")
+	var userModel User
+
+	query := uq.db.Preload("Venues").Preload("Reservations").Preload("Reviews").First(&userModel, userID)
+	if query.Error != nil {
+		if errors.Is(query.Error, gorm.ErrRecordNotFound) {
+			return user.UserEntity{}, fmt.Errorf("user not found with ID: %s", userID)
+		}
+		return user.UserEntity{}, fmt.Errorf("failed to query user: %w", query.Error)
+	}
+
+	userEntity := UserModelToEntity(userModel)
+	return userEntity, nil
 }
 
 // Login implements user.UserData.
 func (uq userQuery) Login(email string, password string) (user.UserEntity, string, error) {
-	panic("unimplemented")
+	var userModel User
+
+	// Check if the email exists in the database
+	query := uq.db.Where("email = ?", email).First(&userModel)
+	if query.Error != nil {
+		if errors.Is(query.Error, gorm.ErrRecordNotFound) {
+			return user.UserEntity{}, "", errors.New("login failed: invalid email")
+		}
+		return user.UserEntity{}, "", fmt.Errorf("failed to query user: %w", query.Error)
+	}
+
+	// Compare the provided password with the stored password
+	err := helper.ComparePass([]byte(userModel.Password), []byte(password))
+	if err != nil {
+		return user.UserEntity{}, "", errors.New("login failed: invalid password")
+	}
+
+	// Generate an access token
+	accessToken, err := middlewares.GenerateToken(userModel.UserID)
+	if err != nil {
+		return user.UserEntity{}, "", fmt.Errorf("failed to generate access token: %w", err)
+	}
+
+	userEntity := UserModelToEntity(userModel)
+	return userEntity, accessToken, nil
 }
 
 // UpdateByID implements user.UserData.
 func (uq userQuery) UpdateByID(userID string, updatedUser user.UserEntity) error {
-	panic("unimplemented")
+	var userModel User
+
+	// Retrieve the user from the database
+	query := uq.db.First(&userModel, userID)
+	if query.Error != nil {
+		if errors.Is(query.Error, gorm.ErrRecordNotFound) {
+			return fmt.Errorf("user not found with ID: %s", userID)
+		}
+		return fmt.Errorf("failed to query user: %w", query.Error)
+	}
+
+	// Hash the updated password if provided
+	if updatedUser.Password != "" {
+		updatedUser.Password = helper.HashPass(updatedUser.Password)
+	}
+
+	// Convert the updated user entity to a model
+	userToUpdate := UserEntityToModel(updatedUser)
+
+	// Perform the update operation
+	update := uq.db.Model(&userModel).Updates(userToUpdate)
+	if update.Error != nil {
+		return fmt.Errorf("failed to update user: %w", update.Error)
+	}
+
+	return nil
 }
 
 func New(db *gorm.DB) user.UserData {
