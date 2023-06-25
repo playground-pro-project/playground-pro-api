396950d626f95696139a0515b3d27cd833c818a9
dimasyudhana <dimas.yudhana@gmail.com>
2023-06-25 10:31:24 +0700

    api/auth/otp/sendmail

--- utils/email/email.go
+++ utils/email/email.go
@@ -6,21 +6,20 @@ import (
 
 	"github.com/playground-pro-project/playground-pro-api/app/config"
 	"github.com/playground-pro-project/playground-pro-api/app/middlewares"
-	"github.com/pquerna/otp/totp"
 	"gopkg.in/gomail.v2"
 )
 
 var log = middlewares.Log()
 
-func SendOTP(name, email string) {
+func SendOTP(otp_secret, otp_auth_url, email string) {
 	var body bytes.Buffer
 	t := template.New("otp.html")
 	t, err := t.Parse(`
 		<html>
 			<body>
 				<h3>Kode OTP Anda</h3>
-				<p>Hallo {{.Name}}, Apa kabar? Semoga harimu menyenangkan!</p>
-				<p>Kode OTP: {{.OTPCode}}</p>
+				<p>OTPSecret: {{.OTPSecret}}</p>
+				<p>OTPAuthURL: {{.OTPAuthURL}}</p>
 			</body>
 		</html>
 	`)
@@ -28,21 +27,16 @@ func SendOTP(name, email string) {
 		log.Error("error parsing template email")
 		return
 	}
-	key, err := totp.Generate(totp.GenerateOpts{
-		Issuer:      config.GOMAIL_HOST,
-		AccountName: config.GOMAIL_EMAIL,
-		SecretSize:  7,
-	})
 
 	if err != nil {
 		panic(err)
 	}
 	err = t.Execute(&body, struct {
-		Name    string
-		OTPCode string
+		OTPSecret  string
+		OTPAuthURL string
 	}{
-		Name:    name,
-		OTPCode: key.Secret(),
+		OTPSecret:  otp_secret,
+		OTPAuthURL: otp_auth_url,
 	})
 	if err != nil {
 		log.Error("error rendering template email")
